ac47028b294571517541efb6dbcb110589e04190
Luiz Andrade <luuizpaulo1@gmail.com>
2026-04-17 11:51:07 -0300

    feat: dialogue

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 enum State {
 	IDLE,
 	RUN,
-	ATTACK
+	ATTACK,
+	DIALOGUE
 }
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
@@ -14,6 +15,23 @@ var current_state: State = State.IDLE
 var last_direction: Vector2 = Vector2.RIGHT
 var input_direction: Vector2 = Vector2.ZERO
 
+# Drag your .dialogue file into this variable in the Inspector
+@export var dialogue_resource: DialogueResource
+@export var dialogue_start: String = "start"
+
+func _ready() -> void:
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+
+func _on_dialogue_ended(_resource: DialogueResource) -> void:
+	change_state(State.IDLE)
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept"):
+		change_state(State.DIALOGUE)
+		velocity = Vector2.ZERO
+		DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start)
+		
+		
 func _physics_process(delta: float) -> void:
 	handle_input()
 	update_state()
@@ -25,6 +43,8 @@ func handle_input() -> void:
 	input_direction = Input.get_vector("left", "right", "up", "down")
 
 func update_state() -> void:
+	if current_state == State.DIALOGUE:
+		return
 	match current_state:
 		State.IDLE:
 			if input_direction != Vector2.ZERO:
@@ -65,3 +85,4 @@ func play_animation(prefix: String, direction: Vector2) -> void:
 		animated_sprite.play(prefix + "_up")
 	elif direction.y > 0:
 		animated_sprite.play(prefix + "_down")
+		
