27c74aba5461cfff926f786a4d796c85962c389f
R-Dias-Kon <ronaldodmjkon@gmail.com>
2026-04-18 17:30:14 -0300

    Criacao de abstracoes para os data types
    Utils para fazer os povs e os prompt chains. por enquanto estou fazendo o prompt chain maker

---
+++ utils/prompt_chain_maker/items_widget.gd
@@ -0,0 +1,36 @@
+extends VBoxContainer
+
+@onready var item_list: ItemList = $ItemList
+@onready var adicionar: Button = $Adicionar
+@onready var remover: Button = $Remover
+@onready var file_dialog: FileDialog = $FileDialog
+
+var items : Array[Item]
+
+func _physics_process(delta: float) -> void:
+	print(items)
+
+func _add_item(item: Item) -> void:
+	items.append(item)
+	item_list.add_item(item.name, item.image)
+
+func _remove_item_by_name(name: String) -> void:
+	for i in items.size():
+		if items[i].name == name:
+			items.pop_at(i)
+			return
+
+func _on_adicionar_pressed() -> void:
+	file_dialog.popup()
+
+func _on_remover_pressed() -> void:
+	for i in item_list.get_selected_items():
+		_remove_item_by_name(item_list.get_item_text(i))
+	for i in item_list.get_selected_items():
+		item_list.remove_item(i)
+
+
+func _on_file_dialog_file_selected(path: String) -> void:
+	var item : Item = load(path)
+	if item:
+		_add_item(item)
---
+++ utils/prompt_chain_maker/prompt_chain_maker.gd
@@ -0,0 +1,11 @@
+extends Control
+
+#Criar uma cadeia de prompt
+#Desenhar uma cadeia de prompt com setas dizendo para onde ir
+#Adicionar (no prompt) texto
+#Adicionar itens necessários
+#Adicionar condições globais
+#Adicionar opcoes
+#Adicionar itens para dar ao player
+#Adicionar itens para tirar do player
+#Adicionar mudancas as variáveis globais
